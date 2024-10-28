1bd0a13902113fcf555f0163c71347a92765e4bd
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-28 02:25:53 +0000

    refactor tests

--- LessInterest/Tests.LessInterest/SimulatorTest.cs
+++ LessInterest/Tests.LessInterest/SimulatorTest.cs
@@ -11,44 +11,18 @@ public class SimulatorTest
 	[Test]
 	public void InitialSimulation()
 	{
-		var config = Config.Init("config_1");
-		var simulator = new Simulator(config);
-
-		var balancesPt = config.GenerateBalancesPT();
-		var nubankLimit = config.NubankLimit;
-		var c6Limit = config.C6Limit;
-		var installmentsCounts = config.InitialInstallmentsCounts;
-		var installmentsDelays = config.InitialInstallmentsDelays;
-
-		var task = simulator.Process(
-			balancesPt, nubankLimit, c6Limit,
-			installmentsCounts, installmentsDelays
-		);
-		task.Wait();
-		var simulation = task.Result;
-
-		var transposed = simulation.Transpose();
-
-		var resultPath = Path.Combine("results", "result_1.json");
-		var resultJson = File.ReadAllText(resultPath);
-		var result = JsonConvert.DeserializeObject<String[,]>(resultJson)!;
-
-		Assert.That(transposed.GetLength(0), Is.EqualTo(result.GetLength(0)));
-		Assert.That(transposed.GetLength(1), Is.EqualTo(result.GetLength(1)));
-
-		for (var r = 0; r < transposed.GetLength(0); r++)
-		{
-			for (var c = 0; c < transposed.GetLength(1); c++)
-			{
-				Assert.That(transposed[r, c].Value, Is.EqualTo(result[r, c]));
-			}
-		}
+		testConfigAndResult(1);
 	}
 
 	[Test]
 	public void Simulation20241027()
 	{
-		var config = Config.Init("config_2");
+		testConfigAndResult(2);
+	}
+
+	private static void testConfigAndResult(Int32 id)
+	{
+		var config = Config.Init($"config_{id}");
 		var simulator = new Simulator(config);
 
 		var balancesPt = config.GenerateBalancesPT();
@@ -66,7 +40,7 @@ public class SimulatorTest
 
 		var transposed = simulation.Transpose();
 
-		var resultPath = Path.Combine("results", "result_2.json");
+		var resultPath = Path.Combine("results", $"result_{id}.json");
 		var resultJson = File.ReadAllText(resultPath);
 		var result = JsonConvert.DeserializeObject<String[,]>(resultJson)!;
 
